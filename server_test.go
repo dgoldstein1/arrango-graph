df0473960024b24a0c20cbaac0116702e194c0e6
dgoldstein1 <david.goldstein@deciphernow.com>
2019-08-31 17:42:56 -0400

    working on adding test able

--- server_test.go
+++ server_test.go
@@ -5,3 +5,26 @@ import (
 )
 
 func TestAddEdges(t *testing.T) {}
+
+func TestGetEdges(t *testing.T) {
+	type Test struct {
+		Name     string
+		Node     string
+		GetEdges func(string) (error, []string)
+	}
+
+	testTable := []Test{
+		Test{
+			Name: "positive test",
+			Node: "/wiki/test",
+			GetEdges: func(s string) (error, []string) {
+				return nil, []string{"/wiki/test1", "/wiki/test2"}
+			},
+		},
+	}
+	for _, test := range testTable {
+		t.Run(test.Name, func(t *testing.T) {
+
+		})
+	}
+}
