a4cc51a9dcee60c2cde6bf123fff8e914faaf3f3
dgoldstein1 <david.goldstein@deciphernow.com>
2019-08-31 18:07:05 -0400

    able to mock out function, test is failing

--- server_test.go
+++ server_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http/httptest"
 	"testing"
 )
@@ -9,10 +11,14 @@ import (
 func TestAddEdges(t *testing.T) {}
 
 func TestGetEdges(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+
 	type Test struct {
-		Name           string
-		GetEdgesFromDB func(string) (error, []string)
-		Params         []gin.Param
+		Name             string
+		GetEdgesFromDB   func(string) (error, []string)
+		Params           []gin.Param
+		ExpectedCode     int
+		ExpectedResponse string
 	}
 
 	testTable := []Test{
@@ -21,7 +27,9 @@ func TestGetEdges(t *testing.T) {
 			GetEdgesFromDB: func(s string) (error, []string) {
 				return nil, []string{"/wiki/test1", "/wiki/test2"}
 			},
-			Params: []gin.Param{gin.Param{Key: "node", Value: "/wiki/test"}},
+			Params:           []gin.Param{gin.Param{Key: "node", Value: "/wiki/test"}},
+			ExpectedCode:     200,
+			ExpectedResponse: `["/wiki/test1", "/wiki/test2"]`,
 		},
 	}
 	for _, test := range testTable {
@@ -29,9 +37,13 @@ func TestGetEdges(t *testing.T) {
 			// create server object
 			s := Server{nil, nil, nil, test.GetEdgesFromDB}
 			// create context
+			w := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(httptest.NewRecorder())
 			c.Params = test.Params
 			s.GetEdges(c)
+			assert.Equal(t, test.ExpectedCode, w.Code)
+			b, _ := ioutil.ReadAll(w.Body)
+			assert.Equal(t, test.ExpectedResponse, string(b))
 		})
 	}
 }
